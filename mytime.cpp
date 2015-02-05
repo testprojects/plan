d8495ceb81d30a12fc0b141fc061883ad9507871
zykis <zykis@rambler.ru>
2015-02-05 21:40:32 +0300

    MyTime redifine to work more accurate (with float type)

--- mytime.cpp
+++ mytime.cpp
@@ -1,27 +1,27 @@
 #include "mytime.h"
 #include <QString>
 
-MyTime::MyTime(): m_days(0), m_hours(0), m_minutes(0)
+MyTime::MyTime(): m_days(0.0), m_hours(0.0), m_minutes(0.0)
 {
 }
 
-MyTime::MyTime(int days, int hours, int minutes): m_days(days), m_hours(hours), m_minutes(minutes)
+MyTime::MyTime(float days, float hours, float minutes): m_days(days), m_hours(hours), m_minutes(minutes)
 {
     adjust();
 }
 
-MyTime MyTime::timeFromHours(int hours)
+MyTime MyTime::timeFromHours(float hours)
 {
-    int days = hours / 24;
+    int days = (int)(hours / 24.0);
     hours -= days * 24;
     return MyTime(days, hours, 0);
 }
 
-MyTime MyTime::timeFromMinutes(int minutes)
+MyTime MyTime::timeFromMinutes(float minutes)
 {
-    int days = minutes / 1440;
+    int days = (int) (minutes / 1440.0);
     minutes -= days * 1440;
-    int hours = minutes / 60;
+    int hours = (int) (minutes / 60.0);
     minutes -= hours * 60;
     return MyTime(days, hours, minutes);
 }
@@ -69,21 +69,21 @@ bool MyTime::operator >=(const MyTime t) const
 MyTime::operator QString () const
 {
     return QString::fromUtf8("%1:%2:%3(%4ч.)")
-            .arg(m_days + 1)
-            .arg(m_hours)
-            .arg(m_minutes)
+            .arg(static_cast<int>(m_days + 1))
+            .arg(static_cast<int>(m_hours))
+            .arg(static_cast<int>(m_minutes))
             .arg(toHours());
 }
 
 void MyTime::adjust()
 {
     if(qAbs(m_minutes) >= 60) {
-        m_hours += m_minutes / 60;
-        m_minutes = m_minutes % 60;
+        m_hours += (int) (m_minutes / 60.0);
+        m_minutes = m_minutes - 24 * ((int)m_minutes % 60);
     }
     if(qAbs(m_hours) >= 24) {
-        m_days += m_hours / 24;
-        m_hours = m_hours % 24;
+        m_days += (int) (m_hours / 24.0);
+        m_hours = m_hours - 24 * ((int)m_hours % 24);
     }
 
     if(m_minutes < 0) {
--- mytime.h
+++ mytime.h
@@ -4,19 +4,19 @@ class QString;
 class MyTime
 {
 private:
-    int m_days;
-    int m_hours;
-    int m_minutes;
+    float m_days;
+    float m_hours;
+    float m_minutes;
 
 public:
     MyTime();
-    MyTime(int days, int hours, int minutes);// (0д, -15ч, -5мин)
+    MyTime(float days, float hours, float minutes);// (0д, -15ч, -5мин)
 
     void adjust();                              //перевести, чтобы было правильно
 
-    int days() const {return m_days;}
-    int hours() const {return m_hours;}
-    int minutes() const {return m_minutes;}
+    float days() const {return m_days;}
+    float hours() const {return m_hours;}
+    float minutes() const {return m_minutes;}
 
 public:
     int toMinutes() const {return m_days * 24 * 60 + m_hours * 60 + m_minutes;}
@@ -28,8 +28,8 @@ public:
             return m_days * 24 + m_hours + 1;
     }
 
-    static MyTime timeFromHours(int hours);
-    static MyTime timeFromMinutes(int minutes);
+    static MyTime timeFromHours(float hours);
+    static MyTime timeFromMinutes(float minutes);
 
     MyTime operator+ (const MyTime t) const;
     MyTime operator- (const MyTime t) const;
